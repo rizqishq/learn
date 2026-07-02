8dd11f2d367b4319cb816debacacb4643caec85f
haq <210781681+rizqishq@users.noreply.github.com>
2026-07-02 16:02:05 +0700

    connect to postgresql, also learn & implement docker

---
+++ README.md
@@ -0,0 +1,21 @@
+# TINY TASKS API
+
+Simple task management api built with Go while also learning Go.
+
+---
+
+## How to run
+1. Clone the repository
+```bash
+git clone https://github.com/rizqishq/tiny-task
+cd tiny-task
+```
+
+2. Start the server
+```go
+go run main.go
+```
+
+3. Test API endpoint
+
+You can directly open [html documentation](tiny-tasks-documentation.html) file in the browser and test it there. Or, you can import the [openapi yaml](tiny-tasks-documentation.yaml) file to your HTTP testing tools like Postman, Bruno, etc.
--- main.go
+++ main.go
@@ -1,22 +1,59 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
-	"fmt"
+	"log"
 	"net/http"
+	"os"
+	"time"
+
+	"github.com/jackc/pgx/v5/pgxpool"
+	_ "github.com/joho/godotenv/autoload"
 )
 
-func main() {
-	http.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusOK)
+func writeJSON(w http.ResponseWriter, code int, data any) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+	json.NewEncoder(w).Encode(data)
+}
 
-		resp := map[string]string{
-			"status": "ok",
+func main() {
+	dsn := os.Getenv("DATABASE_URL")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	db, err := pgxpool.New(ctx, dsn)
+	if err != nil {
+		log.Fatal("Failed to create connection pool: ", err)
+	}
+	defer db.Close()
+
+	if err = db.Ping(ctx); err != nil {
+		log.Fatal("Failed to connect to database: ", err)
+	}
+
+	log.Println("Database connected succesfully")
+
+	http.HandleFunc("GET /health", func(w http.ResponseWriter, r *http.Request) {
+		ctx, cancel := context.WithTimeout(r.Context(), 2*time.Second)
+		defer cancel()
+
+		if err := db.Ping(ctx); err != nil {
+			resp := map[string]string{
+				"status":   "error",
+				"database": "disconnected",
+			}
+			writeJSON(w, http.StatusServiceUnavailable, resp)
 		}
-		json.NewEncoder(w).Encode(resp)
+		resp := map[string]string{
+			"status":   "ok",
+			"database": "connected",
+		}
+		writeJSON(w, http.StatusOK, resp)
 	})
 
-	fmt.Println("Server running on port :6969")
+	log.Println("Server running on port :6969")
 	http.ListenAndServe(":6969", nil)
 }
